43826aaaa3fd55ce8e3a637ccc0ae9488936dd2f
chronarchitect <aniketdas674@gmail.com>
2019-06-28 12:22:25 +0530

    Added typing animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,8 @@
-var i = 0;
-var txt = " ";
-var speed = 90;
-
-window.onload = function() {
-	txt = document.getElementById("typewriter").textContent;
-	clearWriter();
-	typeWriter();
+var options = {
+  strings: ["Undergrad", "Enthusiast", "Developer"],
+  typeSpeed: 40,
+	loop: true,
+	backSpeed: 40
 }
 
-function clearWriter() {
-	document.getElementById("typewriter").innerHTML = "";
-}
-
-function typeWriter() {
-	if (i < txt.length) {
-		document.getElementById("typewriter").innerHTML += txt.charAt(i);
-		i++;
-		speed = 90;
-		if ( i == txt.length){
-			clearWriter();
-			i = 0;
-		}
-		setTimeout(typeWriter, speed);
-	}
-	}
+var typed = new Typed(".typed", options);
